8f69f8cbb89581a599d5ea3249f532eb079c840c
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-08-19 17:53:38 -0300

    Adequa os prints do primeiro dia ao padrão adotado nos outros dias.

--- 1/1.py
+++ 1/1.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #Desafio do dia 01/12/2020
 #a)Receber uma lista de números, e verificar qual par tem soma 2020
 #b)Verificar qual trio tem soma 2020
@@ -19,14 +18,12 @@ for chave, valor in dicionarioModulos.items():
 		a = [(x,y) for x in dicionarioModulos[10-chave] for y in valor if x+y==2020]
 		if a:
 			parEncontrado = a[0]
-			print(f"Encontrado os valores: {parEncontrado[0]} e {parEncontrado[1]}.")
-			print(f"Multiplicação: {parEncontrado[0]*parEncontrado[1]}")
+			print("Encontrado os valores:", parEncontrado[0], "e", parEncontrado[1])
+			print("Multiplicação:", parEncontrado[0]*parEncontrado[1])
 			break
-
-print("Segunda parte:")
-
+#Segunda parte:
 b = [(x,y,z) for x in numeros for y in numeros for z in numeros if (x+y+z)==2020]
 if b:
 	trioEncontrado = b[0]
-	print(f"Encontrado os valores: {trioEncontrado[0]}, {trioEncontrado[1]} e {trioEncontrado[2]}.")
-	print(f"Multiplicação: {trioEncontrado[0] * trioEncontrado[1] * trioEncontrado[2]}.")
+	print("Encontrado os valores:", trioEncontrado[0], trioEncontrado[1], "e", trioEncontrado[2])
+	print("Multiplicação:", trioEncontrado[0] * trioEncontrado[1] * trioEncontrado[2])
