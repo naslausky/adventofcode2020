eac2a1940f783a80d2ba01fc01b287e6cae7c138
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-08-26 23:40:41 -0300

    Adiciona a resolução da segunda parte do décimo nono dia.

--- 19/19.py
+++ 19/19.py
@@ -51,40 +51,34 @@ numeroDeMensagensQueSatisfazemARegra0 = sum([mensagem in dicionarioRegras[0] for
 print("Número de mensagens que satisfazem a regra 0:", numeroDeMensagensQueSatisfazemARegra0)
 
 #Parte 2:
-#Somente a regra 0 utiliza as regras 8 e 11. Essas em suas versões modificadas só possibilitam mensagens maiores.
-#Verificar quais mensagens são maiores do que o tamanho usual:
-tamanhoDasMensagensPossiveis = [len(mensagem) for mensagem in dicionarioRegras[0]]
-tamanhoMensagem = tamanhoDasMensagensPossiveis[0]
-mensagensMaiores = [mensagem for mensagem in linhasMensagens if len(mensagem)>tamanhoMensagem]
-mensagensMaioresComTamanhoMultiploDe8 = [mensagem for mensagem in mensagensMaiores if len(mensagem)%8==0]
-tamanhoMaiorMensagem = max([len(mensagem) for mensagem in mensagensMaiores])
-tamanhoRegra42 = [len(mensagem) for mensagem in dicionarioRegras[42]][0]
+#A estratégia aqui foi: 
+#1) Perceber que as regras recursivas são: -(N múltiplas strings que satisfazem a regra 42 seguidas)
+# e (M mensagens que adequam-se a regras 42 ao lado do mesmo número M de mensagens para as regras 31)
+#2) Como a regra zero precisa das duas regras acima seguidas, logo, são (M+N) strings que satisfazem a 42, mais M que satisfazem a 31.
+#3) Para cada mensagem, ir removendo regras 42 até onde der, depois remover as 31 e ver se a quantidade de remoções é plausível (M>N>0).
+
+tamanhoRegra42 = [len(mensagem) for mensagem in dicionarioRegras[42]][0]  #Para adequar-se aos casos exemplo também, por exemplo
 tamanhoRegra31 = [len(mensagem) for mensagem in dicionarioRegras[31]][0]
-tamanho31Mais42 = tamanhoRegra42 + tamanhoRegra31
-novasMensagensPossiveis = set()
-for mensagemOriginal in dicionarioRegras[42]:
-	for i in range(1,int((tamanhoMaiorMensagem/tamanhoRegra42)+1)):
-		novasMensagensPossiveis.add(mensagemOriginal*i)
-#	for mensagemOriginal31 in dicionarioRegras[31]:
-#		for i in range (1,tamanhoMaiorMensagem//tamanho31Mais42):
-#			novasMensagensPossiveis.add((mensagemOriginal*i) + (mensagemOriginal31*i))
+numeroDeMensagensQueSatisfazemARegra0 = 0
+for mensagem in linhasMensagens:
+	palavraReduzida = mensagem
+	remocoesDo42 = remocoesDo31 = 0 #Contabilizar o número.
+	while True: #Vai removendo as regras 42 do começo da mensagem até não poder mais, contabilizando o número de vezes.
+		if palavraReduzida[:tamanhoRegra42] in dicionarioRegras[42]:
+			palavraReduzida = palavraReduzida[tamanhoRegra42:]
+			remocoesDo42 += 1
+		else:
+			break
 
-#Tentativa 2:
-mensagensValidas = set()
-for mensagem in mensagensMaiores:
-	for mensagemPossivel42 in dicionarioRegras[42]:
-		if mensagemPossivel42 in mensagem: #
-			for i in range(1,int(tamanhoMaiorMensagem/tamanhoRegra42)+1):
-				#print(mensagemPossivel42*i)
-				if mensagem == (mensagemPossivel42*i):#Mensagem Valida
-					#Por algum motivo a primeira regra não encaixa em nenhuma????
-					mensagensValidas.add(mensagem)
-	for mensagemPossivel11 in dicionarioRegras[11]:
-		if mensagemPossivel11 in mensagem:
-			pass
-			#Essa Mensagem	tem o miolo uma mensagem do 11
-#print(mensagensMaiores)
-#input()
-numeroDeMensagensQueSatisfazemARegra0Recursiva = sum([mensagem in novasMensagensPossiveis for mensagem in mensagensMaiores])
+	while True: #O que sobrar, precisa satisfazeri múltiplas vezes a regra 31 no final:
+		if palavraReduzida[-tamanhoRegra31:] in dicionarioRegras[31]:
+			palavraReduzida = palavraReduzida[:-tamanhoRegra31]
+			remocoesDo31 += 1
+		else:
+			break
 
-print("OutroNumero", numeroDeMensagensQueSatisfazemARegra0Recursiva)
+	if not palavraReduzida: #Não pode ter sobrado nada no final.
+		if remocoesDo42 > 0 and remocoesDo31 > 0: #Precisa ter feito no mínimo 1 remoção de cada
+			if remocoesDo42 > remocoesDo31: #Precisa ter removido mais do 42 pois está presente na regra 8 e na 11.
+				numeroDeMensagensQueSatisfazemARegra0 += 1	
+print("Número de mensagens que satisfazem a regra 0 após a substituição das regras:", numeroDeMensagensQueSatisfazemARegra0)
